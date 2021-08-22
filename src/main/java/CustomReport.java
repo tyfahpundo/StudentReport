4caa4b689674c6a3c33ad1f43abe4f1068c3c15e
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-08-22 07:45:55 +0200

    Added a few functions

--- src/main/java/CustomReport.java
+++ src/main/java/CustomReport.java
@@ -1,10 +1,13 @@
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
 import java.util.*;
 
 public class CustomReport {
 
     public static void main(String[] args) {
         try{
-            String filePath ="/home/tyfah/IdeaProjects/FirstReport/src/main/resources/Student.jrxml";
+            String filePath ="/home/tyfah/IdeaProjects/StudentReport/src/main/resources/Student.jrxml";
             //Creating objects of Subject
             Subject sub1 = new Subject("Java", 89);
             Subject sub2 = new Subject("MySql", 95);
@@ -20,11 +23,19 @@ public class CustomReport {
             list.add(sub4);
             list.add(sub5);
             //Passing The list to the JasperReport
-            //Compile the The jasperReport
-            //Convert to pdf
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(list);
 
             Map<String, Object> parameters = new HashMap<String, Object>();
             parameters.put("studentName","Tafadzwa");
+            parameters.put("tableData", dataSource);
+            //Compile the The jasperReport
+            net.sf.jasperreports.engine.JasperReport report = JasperCompileManager.compileReport(filePath);
+            JasperPrint print = JasperFillManager.fillReport(report,parameters,new JREmptyDataSource());
+            //Convert to pdf
+            JasperExportManager.exportReportToPdfFile(print,"/home/tyfah/IdeaProjects/student.pdf");
+
+            System.out.println("Report Created......");
+
 
         }catch(Exception e){
             System.out.println("Error While Generating the Report.....");
